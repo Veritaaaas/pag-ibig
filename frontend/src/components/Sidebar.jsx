5e3b21da53d0cfe8cf3bd299c65809e8be7ab219
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-30 17:33:15 +0800

    Add Sidebar component

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,38 @@
+import { MdOutlinePeopleOutline } from "react-icons/md";
+import { FaRegBuilding } from "react-icons/fa";
+import { BsBuildings } from "react-icons/bs";
+import { MdFaceUnlock } from "react-icons/md";
+
+import Logo from '../assets/logo.png';
+
+function Sidebar() {
+
+    return (
+        <div className="min-h-screen max-w-sm border-r-2 px-6 py-8">
+            <div className='flex items-center justify-start gap-2'>
+                <img src={Logo} alt="logo" className="w-12 h-12" />
+                <h1 className='m-0 text-[20px] font-bold'>Pag-Ibig Database</h1>
+            </div>
+            <div className="flex flex-col mt-16 gap-8">
+                <div className="flex items-center gap-16">
+                    <MdOutlinePeopleOutline className='text-[30px]' />
+                    <h1 className="text-[16px] font-semibold">Employee</h1>
+                </div>
+                <div className="flex items-center gap-16">
+                    <FaRegBuilding className='text-[30px]' />
+                    <h1 className="text-[16px] font-semibold">Present Employers</h1>
+                </div>
+                <div className="flex items-center gap-16">
+                    <BsBuildings className='text-[30px]' />
+                    <h1 className="text-[16px] font-semibold">Previous Employers</h1>
+                </div>
+                <div className="flex items-center gap-16">
+                    <MdFaceUnlock className='text-[30px]' />
+                    <h1 className="text-[16px] font-semibold">Heirs</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
