69a6462c9d329a1d525a767a402fbe12c569ad3c
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-06-30 20:29:26 +0800

    Add Header component and update Sidebar styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
-import { HiMiniMagnifyingGlass } from "react-icons/hi2";
-import { MdOutlinePersonAddAlt } from "react-icons/md";
 
 import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 
 
 function App() {
@@ -9,16 +8,7 @@ function App() {
     <div className="flex">
       <Sidebar />
       <div className="w-full">
-        <div className="flex w-full justify-between pt-8 px-8">
-          <div className="flex text-black items-center gap-4">
-            <input type="text" placeholder="Search" className="border-b-2 border-gray-300 p-2 font-semibold min-w-16 text-[17px]" />
-            <HiMiniMagnifyingGlass className="text-[25px]" />
-          </div>
-          <div className="flex items-center text-[#3564FD] gap-6">
-            <MdOutlinePersonAddAlt className="text-[40px]" />
-            <h1 className="text-[20px] font-bold">ADD EMPLOYEE</h1>
-          </div>
-        </div>
+        <Header />
       </div>
     </div>
   );
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -8,10 +8,10 @@ import Logo from '../assets/logo.png';
 function Sidebar() {
 
     return (
-        <div className="min-h-screen max-w-sm border-r-2 px-6 py-8">
+        <div className="min-h-screen min-w-[15%] border-r-2 px-6 py-8">
             <div className='flex items-center justify-start gap-2'>
                 <img src={Logo} alt="logo" className="w-12 h-12" />
-                <h1 className='m-0 text-[20px] font-bold'>Pag-Ibig Database</h1>
+                <h1 className='m-0 text-[20px] font-bold text-[#3564FD]'>Pag-Ibig Database</h1>
             </div>
             <div className="flex flex-col mt-16 gap-8">
                 <div className="flex items-center gap-16">
